a8f8e1eb87fdee9ab0407ab4807b6e2e9bf5d867
Steven Leija <stevenleija@gmail.com>
2024-05-09 11:26:20 -0500

    Initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-// @ts-check
-
 module.exports = {
     root: true,
     env: {
@@ -11,7 +9,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended'
     ],
-    ignorePatterns: ['dist', '.eslintrc.js'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
