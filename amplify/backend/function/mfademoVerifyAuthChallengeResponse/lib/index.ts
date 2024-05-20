51d3fc718fd886e6c2f97e898369009e0c82a0e4
Steven Leija <stevenleija@gmail.com>
2024-05-20 14:43:09 -0500

    Adding custom auth triggers

---
+++ amplify/backend/function/mfademoVerifyAuthChallengeResponse/lib/index.ts
@@ -0,0 +1,12 @@
+
+
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    const expectedAnswer = event.request.privateChallengeParameters!.secretLoginCode;
+    if (event.request.challengeAnswer === expectedAnswer) {
+        event.response.answerCorrect = true;
+    } else {
+        event.response.answerCorrect = false;
+    }
+    return event;
+};
